1a364e77d2d1f7eb7d904322b8767056935d87ff
Marcin Jerwan <m.jerwan@gmail.com>
2018-09-14 22:56:46 +0100

    can log in console all crimes in area by category

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -2,17 +2,22 @@ const PubSub = require('../helpers/pub_sub');
 const CreateHtmlElem = require('../helpers/createHtmlElem');
 
 
+
 const ResultView = function (htmlElement) {
     this.htmlElement = htmlElement;
 };
 
 ResultView.prototype.bindEvents = function(){
     PubSub.subscribe('Engine:crimes-in-the-area', (event)=>{
-        console.log(event.detail);
+        this.renderCrimes(event.detail);
     })
 }
 
-
+ResultView.prototype.renderCrimes = function (crimeData) {
+    crimeData.forEach(crime => {
+        console.log(crime.category);
+    });
+}
 
 
 
